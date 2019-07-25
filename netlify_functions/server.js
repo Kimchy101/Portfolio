b965a3e04c4395f1e027e47eaec1869ab67df3d7
Kimchy101 <kimchy101@gmail.com>
2019-07-24 23:37:15 -0500

    yet another attempt at a signal over netlify

--- netlify_functions/server.js
+++ netlify_functions/server.js
@@ -5,13 +5,12 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-// router.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write('<h1>Hello from Express.js!</h1>');
-//   res.end();
-// });
 
-router.get('/', (req, res) => res.json('Hello World'))
+router.get('/userPosts', (req, res) => {
+  res.json({name: 'Andrew', quote: 'like pie'})
+});
+
+
 // router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 // router.post('/', (req, res) => res.json({ postBody: req.body }));
 
